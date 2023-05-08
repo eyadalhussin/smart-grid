fe8aa8023c5a75d3cbec83ff3f9034b7cd758b1a
RawRaven <larskoenigsmann@outlook.de>
2023-05-08 16:07:55 +0200

    Added Spring Projekt

---
+++ smart-grid/SmartGrid/src/main/java/de/fhdo/SmartGrid/SmartGridApplication.java
@@ -0,0 +1,13 @@
+package de.fhdo.SmartGrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGridApplication.class, args);
+	}
+
+}
---
+++ smart-grid/SmartGrid/src/test/java/de/fhdo/SmartGrid/SmartGridApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fhdo.SmartGrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
